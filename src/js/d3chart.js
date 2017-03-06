af77db9009c65c6af5114d6109b6f8dbb59f4fc3
amit <shettya@gmail.com>
2017-03-06 11:11:56 -0800

    clean d3chart.js

--- src/js/d3chart.js
+++ src/js/d3chart.js
@@ -6,7 +6,7 @@ const height = 350;
 export  function renderChart(data){	
 	
 	let dataset = data.options.map(option=>Object.assign({},{name: option.name, count: option.count}));
-	let categories = data.options.map(item=>item.name);	
+	//let categories = data.options.map(item=>item.name);	
 
 	let w = width;
 	let h = height;
@@ -62,6 +62,4 @@ export  function renderChart(data){
 			.data(pie(dataset))
 			.exit()
 			.remove();
-
 }
-
