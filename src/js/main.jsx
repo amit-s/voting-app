0066e0818721bc1588aa31412d8da66fa15c476d
amit <shettya@gmail.com>
2017-04-04 08:38:28 -0700

    clean

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {render} from 'react-dom';
-//import ConfirmPoll from './components/ConfirmPoll.jsx';
 import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 import routes from './components/Routes.jsx';
 
